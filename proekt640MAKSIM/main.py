5bb5df0b28e2d5ae799fb46aba36d3593b5fb234
Bogdan <Bogdan@>
2026-03-04 19:39:52 +0500

    New commit bogdan :)

---
+++ proekt640MAKSIM/main.py
@@ -0,0 +1,15 @@
+from proekt640 import funk320FIO
+from funcKosyanenko import funk640BOGDAN
+
+if __name__ == "__main__":
+    a = 5
+    b = 10
+    result = funk320FIO(a, b)
+    print(f"Результат работы функции: {result}")
+
+#Здесь накодил Богдан
+
+x = int(input("Enter your x: "))
+funk640BOGDAN(x)
+
+#=======
