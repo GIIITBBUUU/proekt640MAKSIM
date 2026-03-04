4f866acdb927303896772c8f60b0bc7c6f31a052
AKhmedovc <dakhmedovvv@gmail.com>
2026-03-04 13:30:03 +0500

    Ахмедов добавил

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from proekt640 import funk320FIO
+from FuncAkhmedov import funk640DILMUROD
 
 if __name__ == "__main__":
     a = 5
     b = 10
     result = funk320FIO(a, b)
     print(f"Результат работы функции: {result}")
+
+y = int(input("Введите число y: "))
+funk640DILMUROD(y)
+
